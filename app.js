d0b93759f7135abfc8912dfd6d6a3ce71a318d44
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2022-12-25 15:26:11 -0800

    centered ty container, rating works

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 const ratingBtns = document.getElementsByClassName('rating');
-const numberRating = document.getElementById('ratingNum');
+const numberRatingSpan = document.getElementById('ratingNum');
 const submitBtn = document.getElementById('submitButton');
 
 
-    for (const button of ratingBtns) {
-        button.addEventListener('click', () => {
-            numberRating.innerText = button.innerText;
+for (const button of ratingBtns) {
+    
+    button.addEventListener('click', () => {
+        button.style.backgroundColor = '#7c8798';
+        button.style.color = 'white';
+        numberRatingSpan.innerText = button.innerText;
+        
+    });
+   
+};
 
-            document.getElementsByClassName('main-container')[0].style.display = 'none';
-            document.getElementsByClassName('thankYouContainer')[0].style.display = 'block';
-            let thankStyle = document.getElementsByClassName('thankYouContainer')[0].style;
 
-            thankStyle.display = 'flex';
-            thankStyle.flexDirection = 'column'; 
-            thankStyle.alignItems = 'center'; 
-            thankStyle.justifyContent = 'space-around';
-            thankStyle.backgroundColor = 'var(--darkBlue)';
-            thankStyle.width = 29 + '%';
-            thankStyle.height = 50 + '%';
-            thankStyle.borderRadius = 35 + 'px';
-            thankStyle.padding = 25 + 'px ' + 20 + 'px ' + 25 + 'px ' + 25 + 'px'; 
-            
-        });
-    };
+submitBtn.addEventListener('click', () => {
+
+    document.getElementsByClassName('main-container')[0].style.display = 'none';
+
+    let thankStyle = document.getElementsByClassName('thankYouContainer')[0].style;
+    thankStyle.display = 'flex';
+    thankStyle.flexDirection = 'column'; 
+    thankStyle.alignItems = 'center'; 
+    thankStyle.justifyContent = 'space-around';
+    thankStyle.backgroundColor = 'var(--darkBlue)';
+    thankStyle.width = 29 + '%';
+    thankStyle.height = 53 + '%';
+    thankStyle.borderRadius = 35 + 'px';
+    thankStyle.padding = 25 + 'px ' + 20 + 'px ' + 25 + 'px ' + 25 + 'px'; 
+})
