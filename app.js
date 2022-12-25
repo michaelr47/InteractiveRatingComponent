bdbc21693426cb88283946e500aa64821ff35492
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2022-12-25 14:10:34 -0800

    added functionality on rating buttons, working on ty page

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 const ratingBtns = document.getElementsByClassName('rating');
+const numberRating = document.getElementById('ratingNum');
+const submitBtn = document.getElementById('submitButton');
 
-for (const button of ratingBtns) {
-    button.addEventListener('click', () => {
-        document.getElementsByClassName('main-container')[0].style.display = 'none';
-        document.getElementsByClassName('thankYouContainer')[0].style.display = 'inline';
-        let thankStyle = document.getElementsByClassName('thankYouContainer')[0].style;
 
-        thankStyle.display = 'flex';
-        thankStyle.display.flexDirection = 'column'; 
-        thankStyle.display.justifyContent = 'space-around';
-        thankStyle.display.backgroundColor = 'white';
-        thankStyle.display.width = 29 + '%';
-        thankStyle.display.height = 55 + '%';
-        thankStyle.display.borderRadius = 35 + 'px';
-        thankStyle.display.padding = 25 + 'px ' + 20 + 'px ' + 25 + 'px ' + 25 + 'px'; 
-        
-    });
+    for (const button of ratingBtns) {
+        button.addEventListener('click', () => {
+            numberRating.innerText = button.innerText;
 
-};
+            document.getElementsByClassName('main-container')[0].style.display = 'none';
+            document.getElementsByClassName('thankYouContainer')[0].style.display = 'block';
+            let thankStyle = document.getElementsByClassName('thankYouContainer')[0].style;
 
+            thankStyle.display = 'flex';
+            thankStyle.flexDirection = 'column'; 
+            thankStyle.alignItems = 'center'; 
+            thankStyle.justifyContent = 'space-around';
+            thankStyle.backgroundColor = 'var(--darkBlue)';
+            thankStyle.width = 29 + '%';
+            thankStyle.height = 50 + '%';
+            thankStyle.borderRadius = 35 + 'px';
+            thankStyle.padding = 25 + 'px ' + 20 + 'px ' + 25 + 'px ' + 25 + 'px'; 
+            
+        });
+    };
