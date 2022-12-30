a2c2e19cdfc3f80006879442d71b08a8f54ca1f8
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2022-12-29 17:24:14 -0800

    added new class to ty container, css style to it, and remove 'hidden' class

--- app.js
+++ app.js
@@ -18,15 +18,9 @@ for (const button of ratingBtns) {
 submitBtn.addEventListener('click', () => {
 
     document.getElementsByClassName('main-container')[0].style.display = 'none';
+    document.getElementsByClassName('thankYouContainer')[0].classList.remove('hidden');
 
-    let thankStyle = document.getElementsByClassName('thankYouContainer')[0].style;
-    thankStyle.display = 'flex';
-    thankStyle.flexDirection = 'column'; 
-    thankStyle.alignItems = 'center'; 
-    thankStyle.justifyContent = 'space-around';
-    thankStyle.backgroundColor = 'var(--darkBlue)';
-    thankStyle.width = 29 + '%';
-    thankStyle.height = 53 + '%';
-    thankStyle.borderRadius = 35 + 'px';
-    thankStyle.padding = 25 + 'px ' + 20 + 'px ' + 25 + 'px ' + 25 + 'px'; 
 })
+
+
+
