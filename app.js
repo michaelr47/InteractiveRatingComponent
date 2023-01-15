e83b246c2ba6bf11ed5ede2d89b336812a5032a0
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-01-14 19:15:24 -0800

    fixed bug where button onclick would change all colors of buttons

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const ratingBtns = document.querySelectorAll('.rating');
+
 const numberRatingSpan = document.getElementById('ratingNum');
 const submitBtn = document.getElementById('submitButton');
 
+let currentClickedButton = null;
 
-ratingBtns.forEach(btn => {
-    
-    btn.addEventListener('click', () => {
-
-        btn.style.backgroundColor = 'hsl(25, 97%, 53%)';
-        btn.style.color = 'white';
-        numberRatingSpan.innerText = btn.innerText;
-    })
-})
- 
-   
-
+ratingBtns.forEach(function(button) {
+  button.addEventListener("click", function() {
+    let currentBtn = this;
+    ratingBtns.forEach(function(btn) {
+      btn.style.backgroundColor = "hsl(216, 12%, 8%)";
+      btn.style.color = "hsl(216, 12%, 54%)";
+    });
+    currentBtn.style.backgroundColor = "hsl(25, 97%, 53%)";
+    currentBtn.style.color = "white";
+   numberRatingSpan.innerText = currentBtn.innerText;
+  });
+});
 
 
 let click = false;
